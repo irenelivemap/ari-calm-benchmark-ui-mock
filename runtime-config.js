fa36a19b6a48613f0cb4e60a78bee918652a3bd4
Irene Aguayo <irene.aguayo@livemap.ch>
2026-08-04 16:29:54 +0200

    Connect benchmark to Supabase for participant data collection

--- runtime-config.js
+++ runtime-config.js
@@ -5,6 +5,8 @@
     publicUrl: '',
     routingApiBase: '/api/v1/routing',
     dataApiBase: '',
+    supabaseUrl: 'https://xyrmytymcipyntdtsksu.supabase.co',
+    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh5cm15dHltY2lweW50ZHRza3N1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NDkyNTAsImV4cCI6MjEwMTQyNTI1MH0.eSYOBBKeLWF6lKA51sUcvK5xYjEaXb3yaZk1Mv1euSo',
     googleMapsKey: '',
     allowQueryConfig: true,
     showReset: true,
